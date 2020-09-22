8e50d03eb726085546de91d4094cf320e030b004
Anna Ronkai <anna.ronkai@stud.uni-corvinus.hu>
2020-09-23 00:24:03 +0200

    SaveFile funkció

--- UserMaintenance/UserMaintenance/Entities/User.cs
+++ UserMaintenance/UserMaintenance/Entities/User.cs
@@ -11,6 +11,6 @@ namespace UserMaintenance.Entities
         public Guid ID { get; set; } = Guid.NewGuid();
 
 
-        public string FullName { get; set; };
+        public string FullName { get; set; }
     }
 }
