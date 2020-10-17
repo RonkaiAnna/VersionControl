b2bb2c6e03a6977e67eafb9bc6d2ea1267a7c494
Anna Ronkai <anna.ronkai@stud.uni-corvinus.hu>
2020-10-17 19:45:10 +0200

    Webszolgáltatás hívása week06

--- week06/week06/Form1.cs
+++ week06/week06/Form1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week06.MnbserviceReference;
 
 namespace week06
 {
@@ -15,6 +17,31 @@ namespace week06
         public Form1()
         {
             InitializeComponent();
+            WebszolgaltatoMeghivas();
+        }
+        void WebszolgaltatoMeghivas()
+        {
+            var mnbservice = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbservice.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
+            /*SaveFileDialog sfd = new SaveFileDialog();
+            sfd.InitialDirectory = Application.StartupPath;
+            sfd.DefaultExt = "xml";
+            sfd.AddExtension = true;
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+                sw.Write(result);
+            }*/
+            //XML vizsgálata
         }
     }
 }
